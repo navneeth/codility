73328200367bfd597b51e948e9a457347f1848e5
Navneeth Subramanian <navneeth.s@gmail.com>
2021-09-10 14:35:20 +0900

    Create 01_reverse_string_in_place.py

---
+++ 01_reverse_string_in_place.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+def reverse(list_of_chars):
+    if len(list_of_chars) < 2:
+        return list_of_chars
+    # Reverse the input list of chars in place
+    length = len(list_of_chars)
+
+    for i in range(length//2):
+        list_of_chars[i], list_of_chars[-(i+1)] =  list_of_chars[-(i+1)], list_of_chars[i]
+    pass
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_empty_string(self):
+        list_of_chars = []
+        reverse(list_of_chars)
+        expected = []
+        self.assertEqual(list_of_chars, expected)
+
+    def test_single_character_string(self):
+        list_of_chars = ['A']
+        reverse(list_of_chars)
+        expected = ['A']
+        self.assertEqual(list_of_chars, expected)
+
+    def test_longer_string(self):
+        list_of_chars = ['A', 'B', 'C', 'D', 'E']
+        reverse(list_of_chars)
+        expected = ['E', 'D', 'C', 'B', 'A']
+        self.assertEqual(list_of_chars, expected)
+
+
+unittest.main(verbosity=2)
